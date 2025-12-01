cb0fbb2845083b5d569ad71b5aa0301ea0de178a
TheManiDr <118424374+TheManiDr@users.noreply.github.com>
2025-12-01 13:13:52 -0600

    agregue el login en el main()

--- src/tallerdemotos/TallerDeMotos.java
+++ src/tallerdemotos/TallerDeMotos.java
@@ -14,6 +14,8 @@ public class TallerDeMotos {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        java.awt.EventQueue.invokeLater(() -> {
+            new view.LoginForm().setVisible(true);
+        });
     }
 }
