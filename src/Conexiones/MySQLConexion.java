1be40b961ca3db625baff5206484804f83c002c1
TheManiDr <TheManiDr@DESKTOP-NEMGBKI>
2025-12-01 21:37:54 -0600

    conecxion de base de datos

--- src/Conexiones/MySQLConexion.java
+++ src/Conexiones/MySQLConexion.java
@@ -1,8 +1,11 @@
 package Conexiones; // cambia por tu paquete
 
+import java.io.IOException;
+import java.io.InputStream;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.Properties;
 
 public class MySQLConexion {
 
@@ -11,21 +14,40 @@ public class MySQLConexion {
     public Connection conectar() {
 
         try {
+            Properties p = new Properties();
+            
+            InputStream input = getClass().getClassLoader().getResourceAsStream("config/config.properties");
+            
+            if (input == null) {
+                System.out.println("No se encontró el archivo config.properties");
+                return null;
+            }
+
+            p.load(input);
+
+            String host = p.getProperty("db_host");
+            String port = p.getProperty("db_port");
+            String name = p.getProperty("db_name");
+            String user = p.getProperty("db_user");
+            String pass = p.getProperty("db_pass");
+
             Class.forName("com.mysql.cj.jdbc.Driver");
 
             con = DriverManager.getConnection(
-                    "jdbc:mysql://localhost:3306/motomami",
-                    "root",
-                    "Gatoara09." // tu contraseña si tienes
+                "jdbc:mysql://" + host + ":" + port + "/" + name,
+                user,
+                pass
             );
 
-            System.out.println("Conexión exitosa");
+            System.out.println("Conectado a " + name);
+            return con; 
 
-        } catch (ClassNotFoundException | SQLException e) {
-            System.out.println("Error en la conexión: " + e.getMessage());
+        } catch (IOException | ClassNotFoundException | SQLException e) {
+            System.out.println("ERROR en la conexión: " + e.getMessage());
+            return null;
         }
 
-        return con;
+        
     }
 }
 
